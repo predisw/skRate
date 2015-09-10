3d965f74bfce5da546e628ab0b997ec83e1f85a6
predisw <predisw@sina.com>
2015-09-10 09:03:45 +0800

    delete package

--- action/target/SkylineRate/excel_export/readme.txt
+++
@@ -1,1 +0,0 @@
-this folder is used to store the excel attachment which is exported with mysql data.Then it will be sent to customer via email
--- action/target/SkylineRate/js/notice.js
+++
@@ -1,9 +0,0 @@
-/**
- * 有一种思路是在jsp 页面中设置一个隐藏的input ,id 固定.这个input 的value 读取后台的值;然后在js 文件中读取这个id 的值来输出
- */
-
-/*if('${Message}'!=null && '${Message}'!=""){
-	alert('${Message}');
-//	location.href="${pageContext.request.contextPath}/getRateList.do";
-}
-*/
